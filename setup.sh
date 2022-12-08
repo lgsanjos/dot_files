b07857468bb1ddc438143ca597522dd25e6d5c6f
Luiz Guilherme <lgsanjos@gmail.com>
2022-12-09 10:41:39 +1100

    switch nvchad to astrovim

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 set -x
 
-ln -s ~/dotfiles/config/nvim/nvchad/custom ~/.config/nvim/lua/custom
+# ln -s ~/dotfiles/config/nvim/nvchad/custom ~/.config/nvim/lua/custom
+ln -s ~/dotfiles/nvim/astrovim ~/.config/nvim
 ln -s ~/dotfiles/config/wezterm ~/.config/wezterm
 
 npm install -g spaceship-zsh-theme

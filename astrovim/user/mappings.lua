bb8b3a08d2d61c3190d71387470abaf71c387009
Luiz Guilherme <lgsanjos@gmail.com>
2023-06-07 14:08:38 +1000

    more little things

--- astrovim/user/mappings.lua
+++ astrovim/user/mappings.lua
@@ -32,7 +32,7 @@ return {
     ["<leader>bp"] = { "<cmd>:bprevious<cr>", desc = "Previous tab" },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
-    -- ["<leader>b"] = { name = "Buffers", desc = "Buffers" },
+    ["<leader>b"] = { name = "Buffers", desc = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },

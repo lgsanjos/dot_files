a77e3fc2d981bb6d24014f1a85c105205f6beb29
Luiz Anjos <lgsanjos@gmail.com>
2023-07-17 16:42:13 +1000

    attempt to install js dap

--- astrovim/user/plugins/core.lua
+++ astrovim/user/plugins/core.lua
@@ -2,4 +2,10 @@ return {
   { "goolord/alpha-nvim",      enabled = false },
   { "akinsho/toggleterm.nvim", opts = { terminal_mappings = false } },
   { "rcarriga/nvim-notify",    opts = { timeout = 0 } },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      ensure_installed = { "javascript" }
+    }
+  },
 }

2984c3c26e1f6711e4dc09d36b7bf2fb0b8cb774
Luiz Guilherme <lgsanjos@gmail.com>
2024-09-02 17:51:15 +1000

    comment and remapped join lines

--- nvim2024/lua/me/remap.lua
+++ nvim2024/lua/me/remap.lua
@@ -15,6 +15,8 @@ vim.keymap.set("n", "<leader>ck", "<cmd>e ~/.config/nvim/lua/me/remap.lua<cr>");
 -- PLUGINS --
 --
 
+vim.keymap.set("n", "<leader>gb", "<cmd>GitBlameToggle<cr>");
+
 -- Twilight
 vim.keymap.set("n", "<leader>tt", "<cmd>Twilight<cr>");
 
--- nvim2024/lua/plugins/base.lua
+++ nvim2024/lua/plugins/base.lua
@@ -1,85 +1,110 @@
 return {
-    -- the colorscheme should be available when starting Neovim
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
-            vim.cmd([[colorscheme tokyonight]])
-        end,
+  -- the colorscheme should be available when starting Neovim
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    }
+  },
+
+  {
+    "f-person/git-blame.nvim",
+    -- load the plugin at startup
+    event = "VeryLazy",
+    -- Because of the keys part, you will be lazy loading this plugin.
+    -- The plugin wil only load once one of the keys is used.
+    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
+    -- or lazy = false. One of both options will work.
+    opts = {
+      -- your configuration comes here
+      -- for example
+      enabled = false, -- if you want to enable the plugin
+      message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+      date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+      virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
     },
+  },
 
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+
+  { "tpope/vim-fugitive",          lazy = false },
+
+  -- if some code requires a module from an unloaded plugin, it will be automatically loaded.
+  -- So for api plugins like devicons, we can always set lazy=true
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+
+  -- you can use the VeryLazy event for things that can
+  -- load later and are not important for the initial UI
+  { "stevearc/dressing.nvim",      event = "VeryLazy" },
+
+  {
+    "Wansmer/treesj",
+    keys = {
+      { "<leader>j", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
     },
-
-    { "tpope/vim-fugitive",          lazy = false },
-
-    -- if some code requires a module from an unloaded plugin, it will be automatically loaded.
-    -- So for api plugins like devicons, we can always set lazy=true
-    { "nvim-tree/nvim-web-devicons", lazy = true },
-
-    -- you can use the VeryLazy event for things that can
-    -- load later and are not important for the initial UI
-    { "stevearc/dressing.nvim",      event = "VeryLazy" },
-
-    {
-        "Wansmer/treesj",
-        keys = {
-            { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
-        },
-        opts = { use_default_keymaps = false, max_join_length = 150 },
-    },
+    opts = { use_default_keymaps = false, max_join_length = 150 },
+  },
 
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = function()
-            require("nvim-treesitter.install").update({ with_sync = true })()
-        end,
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+  },
 
-    -- {
-    --     "ThePrimeagen/harpoon",
-    --     branch = "harpoon2",
-    --     dependencies = { "nvim-lua/plenary.nvim" }
-    -- },
+  -- {
+  --     "ThePrimeagen/harpoon",
+  --     branch = "harpoon2",
+  --     dependencies = { "nvim-lua/plenary.nvim" }
+  -- },
 
-    { "anuvyklack/pretty-fold.nvim", lazy = false },
+  { "anuvyklack/pretty-fold.nvim", lazy = false },
 
-    { "ggandor/lightspeed.nvim",     lazy = false },
+  { "ggandor/lightspeed.nvim",     lazy = false },
 
-    {
-        "folke/twilight.nvim",
-        lazy = false,
-        opts = {
-            dimming = {
-                alpha = 0.25, -- amount of dimming
-                -- we try to get the foreground from the highlight groups or fallback color
-                color = { "Normal", "#ffffff" },
-                term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
-                inactive = false,    -- when true, other windows will be fully dimmed (unless they contain the same buffer)
-            },
-            context = 10,            -- amount of lines we will try to show around the current line
-            treesitter = true,       -- use treesitter when available for the filetype
-            -- treesitter is used to automatically expand the visible text,
-            -- but you can further control the types of nodes that should always be fully expanded
-            expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
-                "function",
-                "block",
-                "method",
-                "table",
-                "if_statement",
-            },
-            exclude = {}, -- exclude these filetypes
-        }
-    },
+  {
+    "folke/twilight.nvim",
+    lazy = false,
+    opts = {
+      dimming = {
+        alpha = 0.25, -- amount of dimming
+        -- we try to get the foreground from the highlight groups or fallback color
+        color = { "Normal", "#ffffff" },
+        term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
+        inactive = false,    -- when true, other windows will be fully dimmed (unless they contain the same buffer)
+      },
+      context = 10,          -- amount of lines we will try to show around the current line
+      treesitter = true,     -- use treesitter when available for the filetype
+      -- treesitter is used to automatically expand the visible text,
+      -- but you can further control the types of nodes that should always be fully expanded
+      expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+        "function",
+        "block",
+        "method",
+        "table",
+        "if_statement",
+      },
+      exclude = {}, -- exclude these filetypes
+    }
+  },
 }

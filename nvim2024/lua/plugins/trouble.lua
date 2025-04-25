56a8baed3294ad75a817b27be08f875e42528343
Luiz Anjos <lgsanjos@gmail.com>
2025-04-25 21:02:33 +1000

    trouble

--- nvim2024/lua/plugins/trouble.lua
+++ nvim2024/lua/plugins/trouble.lua
@@ -1,23 +1,31 @@
 return {
-    {
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup({
-                icons = false,
-            })
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy", -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      require('tiny-inline-diagnostic').setup()
+      vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+    end
 
-            vim.keymap.set("n", "<leader>T", function()
-                require("trouble").toggle()
-            end)
-
-            vim.keymap.set("n", "<leader>nt", function()
-                require("trouble").next({skip_groups = true, jump = true});
-            end)
-
-            vim.keymap.set("n", "<leader>pt", function()
-                require("trouble").previous({skip_groups = true, jump = true});
-            end)
-
-        end
-    },
+  -- {
+  --   "folke/trouble.nvim",
+  --   config = function()
+  --     require("trouble").setup({
+  --       icons = false,
+  --     })
+  --
+  --     vim.keymap.set("n", "<leader>tt", function()
+  --       require("trouble").toggle()
+  --     end)
+  --
+  --     vim.keymap.set("n", "[t", function()
+  --       require("trouble").next({skip_groups = true, jump = true});
+  --     end)
+  --
+  --     vim.keymap.set("n", "]t", function()
+  --       require("trouble").previous({skip_groups = true, jump = true});
+  --     end)
+  --
+  --   end
+  -- },
 }

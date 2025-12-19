52e3be4a03adb9fcc7745f0bf5dcbe3a2dfd7b1a
Luiz Guilherme <lgsanjos@gmail.com>
2025-12-19 15:11:50 +1100

    some stuff

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 set -x
 
-mkdir -p ~/.config/
 mkdir -p ~/.config/nvim
 # ln -s ~/dotfiles/config/nvim/nvchad/custom ~/.config/nvim/lua/custom
-mv ~/.config/nvim ~/.config/nvim_bkp
+# mv ~/.config/nvim ~/.config/nvim_bkp
 ln -s ~/dotfiles/nvim2024 ~/.config/nvim
 # ln -s ~/dotfiles/astrovim ~/.config/nvim/lua
-ln -s ~/dotfiles/config/wezterm ~/.config/
+# ln -s ~/dotfiles/config/wezterm ~/.config/
 ln -s ~/dotfiles/config/helix ~/.config/
-ln -s ~/dotfiles/alacritty.toml ~/.alacritty.toml
+# ln -s ~/dotfiles/alacritty.toml ~/.alacritty.toml
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
